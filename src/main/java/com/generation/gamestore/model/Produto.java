379c36d69c934872667422d083be06fd2223f084
Weida Winy <winnyweida@hotmail.com>
2024-07-10 10:55:19 -0300

    Create produto.java
    Criação do package model e da classe produto
    Criacao dos atributos, getters e setters e tabela no mysql

---
+++ src/main/java/com/generation/gamestore/model/Produto.java
@@ -0,0 +1,64 @@
+package com.generation.gamestore.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo nome deve conter entre 5 e 100 caracteres!")
+	private String nome;
+	
+	@NotBlank(message = "O atributo preço é obrigatório")
+	private Float preco;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Float preco) {
+		this.preco = preco;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+}
