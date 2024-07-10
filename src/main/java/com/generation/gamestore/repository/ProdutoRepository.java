61ae58e96ae0f10a8235daa91142897ff06b90d9
Weida Winy <winnyweida@hotmail.com>
2024-07-10 11:27:19 -0300

    Criacao do metodo get para todos os produtos
    Criação da classe controller e adição na classe repository para suportar o método get

--- src/main/java/com/generation/gamestore/repository/ProdutoRepository.java
+++ src/main/java/com/generation/gamestore/repository/ProdutoRepository.java
@@ -1,5 +1,9 @@
 package com.generation.gamestore.repository;
 
-public class ProdutoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.gamestore.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
 }
