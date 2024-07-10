3c6269e4e0fd0ea686bc001bf91f747990bf12a9
Weida Winy <winnyweida@hotmail.com>
2024-07-10 11:43:24 -0300

    Update gamestoreController.java
    Adição do método getbyid

--- src/main/java/com/generation/gamestore/controller/ProdutoController.java
+++ src/main/java/com/generation/gamestore/controller/ProdutoController.java
@@ -3,9 +3,11 @@ package com.generation.gamestore.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,12 @@ public class ProdutoController {
 	@GetMapping
 	public ResponseEntity<List<Produto>> getAll(){
 		return ResponseEntity.ok(produtoRepository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> getById(@PathVariable Long id){
+		return produtoRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 }
