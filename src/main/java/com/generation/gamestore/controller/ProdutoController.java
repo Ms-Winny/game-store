d0f34d74147cced5aeaeed1d69ff32f1b8e4df18
Weida Winy <winnyweida@hotmail.com>
2024-07-10 14:57:00 -0300

    Finalização
    - Adição do relacionamento entre as tabelas
    - Adição de exceptions

--- src/main/java/com/generation/gamestore/controller/ProdutoController.java
+++ src/main/java/com/generation/gamestore/controller/ProdutoController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.gamestore.model.Produto;
+import com.generation.gamestore.repository.CategoriaRepository;
 import com.generation.gamestore.repository.ProdutoRepository;
 
 import jakarta.validation.Valid;
@@ -30,6 +31,9 @@ public class ProdutoController {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping
 	public ResponseEntity<List<Produto>> getAll(){
@@ -50,16 +54,28 @@ public class ProdutoController {
 	
 	@PostMapping
 	public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto){
-		return ResponseEntity.status(HttpStatus.CREATED)
+		if(categoriaRepository.existsById(produto.getCategoria().getId())) {
+			return ResponseEntity.status(HttpStatus.CREATED)
 				.body(produtoRepository.save(produto));
+		}
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "A categoria não existe!", null);
 	}
 	
 	@PutMapping
 	public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto){
-		return produtoRepository.findById(produto.getId())
+		if(produtoRepository.existsById(produto.getId())) {
+			if(categoriaRepository.existsById(produto.getCategoria().getId())) {
+				return produtoRepository.findById(produto.getId())
 				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
 						.body(produtoRepository.save(produto)))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+			}
+			
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "A categoria não existe!", null);
+		}
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		
 	}
 	
 	@ResponseStatus(HttpStatus.NO_CONTENT)
